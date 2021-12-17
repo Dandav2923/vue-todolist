aa947293bc90c7288dc499ebc587d8c22258cfad
Dandav2923 <daniele.emili23@gmail.com>
2021-12-17 12:59:05 +0100

    struttura esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 // alert ('ciao');
+const app = new Vue (
+    {
+        el: '#app',
+        data: [
+            {
+                text: 'Chiamare Franco',
+                boolean: null
+            },
+            {
+                text: 'Chiamare Franco',
+                boolean: null
+            },
+            {
+                text: 'Chiamare Franco',
+                boolean: null
+            },
+            {
+                text: 'Chiamare Franco',
+                boolean: null
+            },
+            {
+                text: 'Chiamare Franco',
+                boolean: null
+            }
+        ]
+        
+    },
+)
